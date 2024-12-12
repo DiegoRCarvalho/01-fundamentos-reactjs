7f4dedfa615fe04ecc4246e8988a7421b07ec38e
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-12 17:23:18 -0300

    criar estilos exclusivos do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 import "./global.css";
+import styles from "./App.module.css"
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Diego" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore rerum corporis, saepe magni aperiam quam quaerat ipsa. Nemo porro natus quas fugit, voluptas ad, quae, nesciunt magnam laborum placeat aspernatur."
-      />
+      <div className={styles.wrapper}>
+        <aside>Sidebar</aside>
+        <main>
+          <Post 
+            author="Diego" 
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore rerum corporis, saepe magni aperiam quam quaerat ipsa. Nemo porro natus quas fugit, voluptas ad, quae, nesciunt magnam laborum placeat aspernatur."
+          />
 
-      <Post 
-        author="Bianca" 
-        content="Um novo post."
-      />
+          <Post 
+            author="Bianca" 
+            content="Um novo post."
+          />
+        </main>
+      </div>
     </div>
   )
 }
